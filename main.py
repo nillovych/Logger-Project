8402ff465cc27acc708e463aa28b4113038aff32
Danylo <danykyurkevych@gmail.com>
2024-03-06 22:06:36 +0100

    Added changing of settings' source

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from output_handlers import CsvWriter, TxtWriter, ConsoleWriter
 
-with open('cfg.txt') as cfg:
-    destinations = cfg.read().splitlines()
+answer1 = int(input('Do you want to import destinations from cfg.txt(Enter 0) or write your own(Enter 1): '))
+
+if answer1 == 0:
+    with open('cfg.txt') as cfg:
+        destinations = cfg.read().splitlines()
+elif answer1 == 1:
+    destinations = []
+    answer2 = ''
+    print("Enter 'end', if you want to stop")
+    while answer2 != 'end':
+        answer2 = str(input("Path to file: "))
+        destinations.append(answer2)
+else:
+    print('Choices are 0 and 1')
 
 writers = []
 
