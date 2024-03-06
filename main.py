648b26157190db124a4b13ad7b218bb50f0a1c86
Danylo <danykyurkevych@gmail.com>
2024-03-06 21:38:57 +0100

    Changed the whole program

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from connector import Connector
+from output_handlers import CsvWriter, TxtWriter, ConsoleWriter
+
+with open('cfg.txt') as cfg:
+    destinations = cfg.read().splitlines()
+
+writers = []
+
+for destination in destinations:
+    if destination[-3:] == 'txt':
+        writers.append(TxtWriter(destination))
+    elif destination[-3:] == 'csv':
+        writers.append(CsvWriter(destination))
+    elif destination == 'console':
+        writers.append(ConsoleWriter(destination))
 
 while True:
-    Connector(input('Input Data: '))
+    userInput = input('Enter data, you want to proceed: ')
+
+    for writer in writers:
+        writer.write(userInput)
+
--- output_handlers.py
+++ output_handlers.py
@@ -1,18 +1,41 @@
-class Txt:
-    @staticmethod
-    def output(text, filePath, fileName):
-        with open(filePath + fileName, mode='a') as destination:
+import os
+from abc import ABC, abstractmethod
+
+
+class Writer(ABC):
+    @abstractmethod
+    def write(self, text: str):
+        pass
+
+
+class FileWriter:
+    def __init__(self, path):
+        self.path = path
+
+    def write(self, text):
+        self.file_path = os.path.dirname(self.path)
+
+        if not os.path.exists(self.file_path) and self.file_path != '':
+            os.makedirs(self.file_path)
+
+
+class TxtWriter(FileWriter):
+    def write(self, text):
+        super().write(text)
+        with open(self.path, mode='a') as destination:
             destination.write(text + '\n')
 
 
-class Csv:
-    @staticmethod
-    def output(text, filePath, fileName):
-        with open(filePath + fileName, mode='a') as destination:
+class CsvWriter(FileWriter):
+    def write(self, text):
+        super().write(text)
+        with open(self.path, mode='a') as destination:
             destination.write(text + ',' + '\n')
 
 
-class Console:
-    @staticmethod
-    def output(text):
+class ConsoleWriter:
+    def __init__(self, path):
+        self.path = path
+
+    def write(self, text):
         print(text)
